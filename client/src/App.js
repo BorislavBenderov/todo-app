830ae058b5aeb81841dffa0d12e8e9ca49420f29
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-31 22:29:24 +0200

    Fetch data from server and show todos dynamically

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+    const [todos, setTodos] = useState([]);
+    const [popupActive, setPopupActive] = useState(false);
+    const [newTodo, setNewTodo] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:3001/todos')
+            .then(res => res.json())
+            .then(data => setTodos(data))
+            .catch(err => console.log(err))
+    }, []);
+
     return (
         <div className="App">
             <h1>Welcome User</h1>
             <h4>Your Tasks</h4>
 
             <div className="todos">
-                <div className="todo">
-                    <div className="checkbox">
-                        <div className="text">Go play football</div>
+                {todos.map(todo =>
+                    <div className={"todo" + (todo.complete ? 'is-complete' : '')} key={todo._id}>
+                        <div className="checkbox"></div>
+                        <div className="text">{todo.text}</div>
                         <div className="delete-todo">x</div>
-                    </div>
-                </div>
-                <div className="todo is-complete">
-                    <div className="checkbox">
-                        <div className="text">Go play football</div>
-                        <div className="delete-todo">x</div>
-                    </div>
-                </div>
+                    </div>)}
             </div>
         </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDom.render(
-    <App />,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+        <App />  
 );
